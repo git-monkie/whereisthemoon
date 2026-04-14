e9b53b8182f5404b9b239538d94217eed5c801da
git-monkie <git.monkie@gmail.com>
2026-04-14 11:14:22 +0900

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -126,8 +126,10 @@ function App() {
         playsInline
         style={{
           position: "fixed",
-          width: "100%",
-          height: "100%",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
           objectFit: "cover",
           zIndex: 0,
         }}
